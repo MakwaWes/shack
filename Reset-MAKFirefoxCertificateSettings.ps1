98fb9317a73ed6cf033efb13d2ab2c6c4f832a64
Wesley De Arburn <wesley.de-arburn@ubisoft.com>
2015-09-14 09:21:50 -0400

    Reset Firefox certificate config

---
+++ Reset-MAKFirefoxCertificateSettings.ps1
@@ -0,0 +1,13 @@
+﻿Function Reset-MAKFirefoxCertificateSettings{
+    # Certificate DB and exceptions reset
+    If(Get-Process -Name "firefox" -ErrorAction SilentlyContinue){Stop-Process -Name "firefox"}
+    Get-ChildItem -Path "$Env:APPDATA\Mozilla\Firefox\Profiles" -Recurse -Include "cert8.db","cert_override.txt" | ForEach-Object {
+        If(Get-ChildItem -Path "$Env:APPDATA\Mozilla\Firefox\Profiles" -Recurse -Include "$($_.Name).old" -ErrorAction SilentlyContinue){
+            Remove-Item -Path "$Env:APPDATA\Mozilla\Firefox\Profiles" -Recurse -Include "$($_.Name).old"
+        }
+        Rename-Item -Path $_ -NewName "$($_.Name).old"
+    }
+}
+
+Read-Host -Prompt "Press a key to continue and reset the Firefox settings."
+Reset-MAKFirefoxCertificateSettings
